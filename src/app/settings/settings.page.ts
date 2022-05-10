25da70fe3e3234baffc61c19dc4014a35a3e08c7
takeYY <t0k6yist@gmail.com>
2022-05-10 17:29:56 +0900

    🎨 (settings): title追加

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  title: string = '設定';
   constructor() {}
 
   ngOnInit() {}
