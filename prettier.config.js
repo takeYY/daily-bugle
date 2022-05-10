39dc00953d9716c2ee1aa87671ca488fd8d1bba1
takeYY <t0k6yist@gmail.com>
2022-05-10 14:24:02 +0900

    ➕ husky, lint, prettier追加

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  /**
+   * 1行80文字は狭すぎるので120文字に拡張
+   */
+  printWidth: 120,
+
+  /**
+   * TypeScriptはシングルクォーテーション（'）に統一
+   */
+  singleQuote: true,
+
+  /**
+   * 末尾のカンマはすべてにいれるように
+   */
+  trailingComma: "all",
+
+  /**
+   * アロー関数の単一パラメーターは常にカッコ書きに
+   */
+  arrowParens: "always",
+};
