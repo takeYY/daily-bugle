d02094f52793a1a671fee22a467b6dced20109b5
takeYY <t0k6yist@gmail.com>
2022-05-10 14:56:05 +0900

    🎨 (app): ホームタブ追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public appPages = [{ title: 'ホーム', url: '/home', icon: 'home' }];
   constructor() {}
 }
