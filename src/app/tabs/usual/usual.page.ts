6fd4ed63d6a93824e4665399b440df291abfc6c7
takeYY <t0k6yist@gmail.com>
2022-05-11 18:34:14 +0900

    ✨ (usual): 日常を一覧表示する機能追加

--- src/app/tabs/usual/usual.page.ts
+++ src/app/tabs/usual/usual.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-usual',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
 })
 export class UsualPage {
   title: string = '日常';
+  scene: string = 'everyday';
+  dummy_everyday_contents = ['朝食を摂る', '掃除をする', '髪を整える', '夕飯を作る'];
+  dummy_week_contents = ['ベッドシーツを替える', '掃除機をかける'];
+  dummy_weekday_contents = ['ゴミ出し', '洗濯'];
+
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
   constructor() {}
+
+  segmentChanged(event: any) {
+    console.log(event);
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
+
+  doReorder(event: any) {
+    if (this.scene == 'everyday') {
+      this.dummy_everyday_contents = event.detail.complete(this.dummy_everyday_contents);
+    } else if (this.scene == 'week') {
+      this.dummy_week_contents = event.detail.complete(this.dummy_week_contents);
+    } else {
+      this.dummy_weekday_contents = event.detail.complete(this.dummy_weekday_contents);
+    }
+  }
 }
