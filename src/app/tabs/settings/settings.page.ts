bac6015b8fdd5a65a96191eb279da4eb7e6df3bf
takeYY <t0k6yist@gmail.com>
2022-05-11 13:40:46 +0900

    ✨ (signout): ログアウト機能実装

--- src/app/tabs/settings/settings.page.ts
+++ src/app/tabs/settings/settings.page.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
-export class SettingsPage implements OnInit {
-  title: string = '設定';
-  constructor() {}
+export class SettingsPage {
+  title: string;
+  contents: {
+    label: string;
+    icon: string;
+    action: string;
+  }[];
 
-  ngOnInit() {}
+  constructor(public auth: AuthService, public modalController: ModalController) {
+    this.title = '設定';
+    this.contents = [
+      { label: 'プロフィール', icon: 'person', action: 'openProfile' },
+      { label: 'ログアウト', icon: 'log-out', action: 'signOut' },
+    ];
+  }
+
+  action_router(action_type: string) {
+    switch (action_type) {
+      case 'signOut':
+        return this.signOut();
+      default:
+        break;
+    }
+  }
+
+  signOut() {
+    this.auth.authSignOut();
+  }
 }
