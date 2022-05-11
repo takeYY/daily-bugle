4011f50256b1ba60ac102f5e005705e97d15a47d
takeYY <t0k6yist@gmail.com>
2022-05-11 10:18:26 +0900

    📝 アプリ名の変更

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,9 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'daily-horn',
+  appName: 'daily-bugle',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
