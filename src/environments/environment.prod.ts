cc2a2526205c5794b2ba1b4bc8a6c742f623db6a
takeYY <t0k6yist@gmail.com>
2022-05-18 13:23:44 +0900

    📝 firebase用の環境変数追加

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCDzAfvwTkPMm1lDgtgxRSahclDO53go7U',
+    authDomain: 'daily-bugle-2de82.firebaseapp.com',
+    projectId: 'daily-bugle-2de82',
+    storageBucket: 'daily-bugle-2de82.appspot.com',
+    messagingSenderId: '660937532508',
+    appId: '1:660937532508:web:4a3a6e589f2c5b383bf299',
+  },
+  apiUrl: 'https://asia-northeast1-daily-bugle-2de82.cloudfunctions.net',
+};
