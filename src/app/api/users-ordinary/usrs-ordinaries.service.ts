396b5d2d61900c03450694148a14983137bfa8a7
takeYY <t0k6yist@gmail.com>
2022-05-19 18:01:32 +0900

    ✨ (users-ordinaries): uidで全ての日常を取得する通信追加

--- src/app/api/users-ordinary/usrs-ordinaries.service.ts
+++ src/app/api/users-ordinary/usrs-ordinaries.service.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, retry } from 'rxjs/operators';
 
 import { ConnectService } from '../connect.service';
+import { ErrorService } from '../error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsrsOrdinariesService extends ConnectService {
   basePath: string = this.basePath + '/api/users-ordinaries';
+  http: HttpClient = this.http;
+  errorService: ErrorService = this.errorService;
+
+  findAllByUid(uid: string) {
+    const params = { params: { uid: uid } };
+    return this.http.get(`${this.basePath}/list`, params).pipe(retry(2), catchError(this.errorService.handleError));
+  }
 }
