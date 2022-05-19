921fe71dbc73dd79f08ee2bb551e70e66cc39288
takeYY <t0k6yist@gmail.com>
2022-05-19 17:56:57 +0900

    🏷️ (IUsersOrdinary): 日常と曜日を小要素に追加

--- src/app/interfaces/users-ordinary/IUsersOrdinary.ts
+++ src/app/interfaces/users-ordinary/IUsersOrdinary.ts
@@ -1,8 +1,12 @@
+import { IOrdinary } from '../ordinary/IOrdinary';
+import { IWeekday } from '../weekday/IWeekday';
+
 export interface IUsersOrdinary {
   userId: string;
-  ordinaryId: string;
-  weekdayId: string;
+  ordinary: IOrdinary;
+  weekdays: IWeekday[];
   startedOn: Date;
   createdAt: Date;
   updatedAt: Date;
+  isClosed: boolean;
 }
