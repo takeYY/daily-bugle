b89b7d6beebf257157d331856d654e5983f5b7c3
takeYY <t0k6yist@gmail.com>
2022-05-20 17:21:50 +0900

    🎨 (users-ordinaries): データの処理をserviceへ移行

--- src/app/api/users-ordinary/usrs-ordinaries.service.ts
+++ src/app/api/users-ordinary/usrs-ordinaries.service.ts
@@ -5,6 +5,8 @@ import { catchError, retry } from 'rxjs/operators';
 import { ConnectService } from '../connect.service';
 import { ErrorService } from '../error.service';
 
+import { OrdinariesService } from '../ordinary/ordinaries.service';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,8 +15,62 @@ export class UsrsOrdinariesService extends ConnectService {
   http: HttpClient = this.http;
   errorService: ErrorService = this.errorService;
 
+  constructor(http: HttpClient, errorService: ErrorService, private ordinariesService: OrdinariesService) {
+    super(http, errorService);
+  }
+
   findAllByUid(uid: string) {
     const params = { params: { uid: uid } };
     return this.http.get(`${this.basePath}/list`, params).pipe(retry(2), catchError(this.errorService.handleError));
+  }
+
+  createUesrsOrdinaries(user, ordinary, weekdays, usersOrdinary, ordinariesWeekday) {
+    // TODO: 以下のバリデーションなどを別の場所で処理するように
+    if (!user) {
+      alert('ログインが必要です！');
+      return;
+    }
+
+    if (!ordinary.name) {
+      alert('日常名がありません！');
+      return;
+    }
+    if (
+      !weekdays.filter((w) => {
+        return w.isChecked;
+      })
+    ) {
+      alert('曜日が選択されていません！');
+      return;
+    }
+    // 日常の登録
+    this.ordinariesService.postData(ordinary).subscribe((response) => {
+      // 日常のデータを一時保存
+      const tmpOrdinary = { id: response['id'], name: response['name'] };
+      // 日常の入力項目初期化
+      ordinary.name = '';
+      // 登録する曜日毎にweekdaysを保存し、まとめてusersOrdinaryに追加
+      const tmpWeekday: [] = weekdays.filter((w) => {
+        return w.isChecked;
+      });
+      const tmpWeekdayLength: number = weekdays.filter((w) => {
+        return w.isChecked;
+      }).length;
+      // 雛形作成
+      usersOrdinary.isClosed = false;
+      usersOrdinary.ordinary = tmpOrdinary;
+      usersOrdinary.weekdays = tmpWeekday;
+      this.postData(usersOrdinary).subscribe((res) => {
+        ordinariesWeekday.push({
+          ordinary: tmpOrdinary,
+          weekdays: tmpWeekday,
+          scene: tmpWeekdayLength === 7 ? 'everyday' : tmpWeekdayLength === 1 ? 'week' : 'weekday',
+        });
+        weekdays.map((w) => {
+          w.isChecked = false;
+        });
+      });
+    });
+    return ordinariesWeekday;
   }
 }
