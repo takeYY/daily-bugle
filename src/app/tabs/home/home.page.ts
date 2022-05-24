5f2cd6c746bec7a6b3abe8a69518d66e22c7240d
takeYY <t0k6yist@gmail.com>
2022-05-24 18:20:51 +0900

    💄 (home): 週間毎の達成度と全体の達成度を並列に配置

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -16,40 +16,28 @@ import { first } from 'rxjs/operators';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  // TODO: 折れ線グラフ
-  //@ViewChild('barChart3') barChart3;
-  @ViewChild('barChart4') barChart4;
+  @ViewChild('radarChart') radarChart;
   @ViewChild('allAchievementsPie') allAchievementsPie;
   @ViewChildren('pr_chart', { read: ElementRef }) chartElementRefs: QueryList<ElementRef>;
 
   title = 'ホーム';
-  uid: string;
-  bars: any = [];
-  charts: Chart[];
-  colorArray: any;
-  datasets = [
-    {
-      type: 'pie',
-      label: '朝食を摂る',
-      labels: ['達成', '未達成'],
-      data: [98.2, 1.8],
-    },
-  ];
-  achievements;
-  achievementsByOrdinary;
+  pieChart: any[] = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 
-  achievementPie: any[] = [{}, {}, {}, {}, {}];
-
+  private uid: string;
   private users;
+  private bars: any = [];
+  private charts: Chart[];
+  private achievements;
+  private achievementsByOrdinary;
 
   constructor(
     private auth: AuthService,
     private userService: UserService,
-    public modalController: ModalController,
+    private modalController: ModalController,
     private achievementsService: AchievementsService,
   ) {}
 
-  async ionViewDidEnter(): Promise<void> {
+  async ionViewWillEnter(): Promise<void> {
     this.uid = await this.auth.getUserId();
     this.userService.getList().subscribe(async (response) => {
       this.users = response;
@@ -64,13 +52,9 @@ export class HomePage {
       });
       await modal.present();
     });
-    await this.achievementsService
-      .findAllByUid(this.uid)
-      .pipe(first())
-      .forEach((response) => {
-        this.achievements = response;
-      });
-    console.log('@achievements', this.achievements);
+
+    await this.getAchievementsByDate(this.uid);
+
     // TODO: バックエンド側でデータを加工する
     const uniqueOrdinary = this.achievements.map((achievement) => {
       return achievement.usersOrdinaries.ordinary.name;
@@ -87,12 +71,24 @@ export class HomePage {
       };
     });
 
-    console.log('@achievementsByOrdinary', this.achievementsByOrdinary);
+    // chartの作成
+    this.createRadarChartByWeek();
+    this.createPieChart();
     this.createBarChart();
   }
 
-  createBarChart() {
-    this.bars[0] = new Chart(this.barChart4.nativeElement, {
+  async getAchievementsByDate(uid: string) {
+    await this.achievementsService
+      .findAllByUid(uid)
+      .pipe(first())
+      .forEach((response) => {
+        this.achievements = response;
+      });
+    return await this.achievements;
+  }
+
+  createRadarChartByWeek() {
+    this.bars[0] = new Chart(this.radarChart.nativeElement, {
       type: 'radar',
       data: {
         labels: ['月', '火', '水', '木', '金', '土', '日'],
@@ -118,11 +114,20 @@ export class HomePage {
         responsive: true,
         maintainAspectRatio: false,
         scales: {},
+        plugins: {
+          title: {
+            display: true,
+            text: '週間毎の達成度（モック）',
+            position: 'bottom',
+          },
+        },
       },
     });
     this.bars[0].canvas.parentNode.style.height = '100%';
     this.bars[0].canvas.parentNode.style.width = '100%';
+  }
 
+  createPieChart() {
     const allAchievementLabels = this.achievementsByOrdinary.map((achievement) => {
       return achievement.name;
     });
@@ -137,8 +142,6 @@ export class HomePage {
         })
         .reduce((sum, len) => sum + len, 0),
     );
-    console.log('@allAchievementLabels', allAchievementLabels);
-    console.log('@allAchievementData', allAchievementData);
     this.bars[1] = new Chart(this.allAchievementsPie.nativeElement, {
       type: 'pie',
       options: {
@@ -149,6 +152,7 @@ export class HomePage {
           title: {
             display: true,
             text: '全体の達成度',
+            position: 'bottom',
           },
         },
       },
@@ -165,7 +169,9 @@ export class HomePage {
     });
     this.bars[1].canvas.parentNode.style.height = '100%';
     this.bars[1].canvas.parentNode.style.width = '100%';
+  }
 
+  createBarChart() {
     this.charts = this.chartElementRefs.map((chartElementRef, index) => {
       return new Chart(chartElementRef.nativeElement, {
         type: 'pie',
@@ -195,40 +201,5 @@ export class HomePage {
         },
       });
     });
-
-    // TODO: 折れ線グラフ
-    /* this.bars[0] = new Chart(this.barChart3.nativeElement, {
-      type: 'line',
-      data: {
-        labels: ['5/5', '5/6', '5/7', '5/8', '5/9', '5/10', '5/11'],
-        datasets: [
-          {
-            label: '日移動平均線',
-            data: [100, 80, 80, 80, 60, 40, 80],
-            borderWidth: 1,
-            tension: 0.4,
-          },
-          {
-            label: '週移動平均線',
-            data: [70, 64, 60, 65, 60, 55, 70],
-            borderWidth: 1,
-            tension: 0.2,
-          },
-          {
-            label: '月移動平均線',
-            data: [77, 82, 85, 81.3333, 80, 77, 80],
-            borderWidth: 1,
-            tension: 0.2,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {},
-      },
-    });
-    this.bars[0].canvas.parentNode.style.height = '100%';
-    this.bars[0].canvas.parentNode.style.width = '100%'; */
   }
 }
